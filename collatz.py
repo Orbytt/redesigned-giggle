2c15a8365865b84796ab1edc8d6342571ee4f2ed
Orbytt <adurlach@oberlin.edu>
2025-03-08 19:42:26 +0000

    v1

---
+++ collatz.py
@@ -0,0 +1,16 @@
+def collatz_sequence(x):
+
+
+
+    seq = [x]
+    if x < 1:
+       return []
+    while x > 1:
+       if x % 2 == 0:
+         x = x // 2
+       else:
+         x = 3 * x + 1 
+       seq.append(x)    # Added line
+    return seq
+x = int(input("What do you want x to be? "))
+print(collatz_sequence(x))
--- encoder.py
+++ encoder.py
@@ -1,1 +1,19 @@
 
+def numlet(string):
+  index = 1 
+  string = string.lower()
+  for letter in "abcdefghijklmnopqrstuvwxyz":
+    if string == letter:
+      number = str(index) + " "
+      return number
+    index = index + 1
+  return ""
+
+def cipher(string):
+  code = ""
+  for a in string:
+    code = code + numlet(a)
+  return code
+
+string = input("Input text! ")
+print(cipher(string))
