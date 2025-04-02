e90f39a6327c051ea1a1b25ab3fd792fa9234aa2
Orbytt <adurlach@oberlin.edu>
2025-04-02 17:45:36 +0000

    the code

--- encoder.py
+++ encoder.py
@@ -1,7 +1,6 @@
 def collatz_sequence(x):
 
     seq = [x]
-    print(type(x))
     if x < 1:
        return []
     while x > 1:
@@ -11,7 +10,6 @@ def collatz_sequence(x):
          x = 3 * x + 1 
        seq.append(x)
     seq = ' '.join(str(y) for y in seq)
-    print(type(seq),seq)
     return seq
 
 def let_to_num(string):
@@ -23,21 +21,28 @@ def let_to_num(string):
       return number
     index = index + 1
   return ""
-def num_to_let(num):
-  for number in range (len(num)):
-    number = number // 26
-    print(int(number))
+def num_to_let(current_no):
+  current_no = int(current_no)
+  if current_no > 26:
+    new_current_no = current_no % 26
+  else:
+    new_current_no = current_no
+  print("the number " + str(current_no) + " currently translates to the divided # "+ str(new_current_no)+ " and the letter "+ chr(ord('a') + new_current_no - 1))
+  return chr(ord('a') + new_current_no - 1)
 
 def cipher(string):
   code = ''
   for a in string:
     code = code + collatz_sequence(int(let_to_num(a)))
   return code
+def decipher(number_list):
+  code = ''
+  for b in number_list:
+    code = code + num_to_let(number_list[int(b)])
+  return code
 
 string = input("Input text! ")
-string = string.replace(" ", "")
-numbers = cipher(string)
-number_list = numbers.split()
+string = string.replace(" ", "") 
+number_list = cipher(string).split()
 print(number_list)
-num_to_let(number_list)
-print(5//26)
+print(decipher(number_list))
